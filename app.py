edcb007bbed86c79945ea1f22909cbc205b485ac
Luiz Matos <luiz.matos@amedigital.com>
2021-04-26 21:23:56 -0300

    feat: removes praw from lambda

--- app.py
+++ app.py
@@ -3,9 +3,9 @@ import os
 import praw
 from datetime import datetime
 from src.db.dynamo import get_last_searched_date, save_last_searched_date
-from src.parsers.reddit_parser import get_submission_data, get_subreddit_data, get_comments
+from src.parsers.reddit_parser import get_submission_data, get_subreddit_data, get_comments, get_submission_data_from_pushshift
 from src.services.reddit_service import insert_comment, insert_submission, insert_subreddit
-from src.integrations.pushshift import get_ids_from_submissions_with_keywords_for_interval
+from src.integrations.pushshift import get_ids_from_submissions_with_keywords_for_interval, get_submissions_with_keywords_for_interval
 from src.utils.time_interval import get_timestamp_interval_for_starting_date
 
 
@@ -16,6 +16,42 @@ DEFAULT_COLLECTIONS = {
 }
 
 DATE_FORMAT = '%Y-%m-%d'
+
+
+def get_all_submissions_from_intervals(subreddit, intervals, keyword = None, size = 500):
+    """Search for a keyword inside a subreddit within time intervals
+    and returns the respective submission ids found.
+
+    Parameters:
+
+    keyword (str): keyword to search
+
+    subreddit (str): subreddit title
+
+    intervals (list of tuple): list of interval objects (tuples) representing starting timestamp and ending timestamp
+
+    size (int) - optional: page size requested to the Pushshift API.
+
+    Returns:
+
+    list: a list of submissions
+    """
+    submissions = []
+
+    for interval in intervals:
+        start_date = datetime.fromtimestamp(interval[0])
+        end_date = datetime.fromtimestamp(interval[1])
+
+        if start_date > end_date:
+            continue
+
+        print(f'Searching keyword within range ({start_date}, {end_date})...')
+
+        new_submissions = get_submissions_with_keywords_for_interval(subreddit, interval, keyword)
+
+        submissions = submissions + new_submissions
+
+    return submissions
 
 
 def lambda_handler(event, context):
@@ -75,83 +111,41 @@ def lambda_handler(event, context):
 
         print(f'Starting search within {datetime.fromtimestamp(interval[0])} - {datetime.fromtimestamp(interval[1])} date range')
 
-        subreddit_submissions_map = {}
+        count = 0
 
         for subreddit in params['subreddits']:
             submission_ids = set()
 
-            print(f'Searching inside "{subreddit}" subreddit...')
+            print(f'Searching/gathering inside "{subreddit}" subreddit...')
 
             if len(params['keywords']) == 0:
-                new_submission_ids = get_ids_from_submissions_with_keywords_for_interval(subreddit, interval)
+                new_submissions = get_all_submissions_from_intervals(subreddit, interval)
 
-                if (len(new_submission_ids) == 0): continue
-
-                submission_ids = submission_ids.union(new_submission_ids)
-            else:
-                for keyword in params['keywords']:
-                    print(f'Searching for "{keyword}" keyword...')
-
-                    new_submission_ids = get_ids_from_submissions_with_keywords_for_interval(subreddit, interval, keyword)
-
-                    if (len(new_submission_ids) == 0): continue
-
-                    submission_ids = submission_ids.union(new_submission_ids)
-
-            subreddit_submissions_map[subreddit] = list(submission_ids)
-
-
-        all_ids = [sub_id for id_list in list(subreddit_submissions_map.values()) for sub_id in id_list]
-        total_submissions = len(all_ids)
-        print(f'{total_submissions} submissions found within the given date range')
-
-        print(f'Starting gathering...')
-
-        reddit = praw.Reddit(
-            client_id=os.getenv('REDDIT_CLIENT_ID'), 
-            client_secret=os.getenv('REDDIT_CLIENT_SECRET'),
-            password=os.getenv('REDDIT_PASSWORD'), 
-            user_agent=os.getenv('REDDIT_USERAGENT'),
-            username=os.getenv('REDDIT_USERNAME')
-        )
-
-        subreddits = list(subreddit_submissions_map.keys())
-        for subreddit_id in subreddits:
-            subreddit = reddit.subreddit(subreddit_id)
-
-            if params['saveSubreddits']:
-                print(f'Saving subreddits')
-                subreddit_data = get_subreddit_data(subreddit)
-                insert_subreddit(
-                    subreddit_data, 
-                    params['subredditsCollection']
-                )
-
-            submissions = subreddit_submissions_map[subreddit_id]
-            no_of_submissions_in_subreddit = len(submissions)
-            print(f'Gathering {no_of_submissions_in_subreddit} posts on "{subreddit.display_name}" subreddit')
-
-            for submission_id in submissions:
-                submission = reddit.submission(submission_id)
-
-                submission_data = get_submission_data(submission)
-                if submission_data is not None:
-                    insert_submission(
-                        submission_data,  
-                        params['submissionsCollection']
-                    )
-
-                if params['saveComments']:
-                    print(f'Saving comments')
-                    comments = get_comments(submission)
-                
-                    for comment in comments:
-                        insert_comment(
-                            comment, 
-                            params['commentsCollection']
+                processed_submissions = list(map(lambda submission: get_submission_data_from_pushshift(submission), new_submissions))
+                for submission in processed_submissions:
+                    if submission is not None:
+                        insert_submission(
+                            submission, 
+                            params['submissionsCollection']
                         )
 
-        print(f'Posts gathered and saved')
+                count += len(new_submissions)
+            else:
+                for keyword in params['keywords']:
+                    print(f'Searching/gathering for "{keyword}" keyword...')
+
+                    new_submissions = get_all_submissions_from_intervals(subreddit, interval, keyword)
+
+                    processed_submissions = list(map(lambda submission: get_submission_data_from_pushshift(submission), new_submissions))
+                    for submission in processed_submissions:
+                        insert_submission(
+                            submission, 
+                            params['submissionsCollection']
+                        )
+
+                    count += len(new_submissions)
+
+        print(f'{count} submissions found and collected with the given keywords ({", ".join(params["keywords"])}) and within the date range ({startDate.date()}, {endDate.date()})')
 
         last_searched_date = datetime.fromtimestamp(interval[1])
         save_last_searched_date(last_searched_date)
